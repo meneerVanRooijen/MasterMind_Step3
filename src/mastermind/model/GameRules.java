788a83378cc3f3a401cd4c4c623ed223d2a19c1f
Telau <vanrooijensoftware@gmail.com>
2016-10-06 09:14:32 +0200

    Add to repository

---
+++ src/mastermind/model/GameRules.java
@@ -0,0 +1,11 @@
+package mastermind.model;
+
+/**
+ * Containing the MasterMind game rules 
+ * @author vanrooijen
+ */
+public class GameRules {
+    
+    public static final boolean codeCorrect = false; 
+    
+}
